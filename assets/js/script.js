51b1fc1faee30f59048622dbd84261713fb93635
AMillsy <46033976+AMillsy@users.noreply.github.com>
2023-05-10 13:48:56 +0100

    Showing the current day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 const form = document.querySelector(`form`);
 const searchBtn = document.querySelector(`button`);
 const previousLocations = document.querySelector(`.previous-locations`);
+const currentDayForecast = document.querySelector(`#current-day-forecast`);
+
+const currentDayLocation = document.querySelector(`#current-day-location`);
 
 const API_KEY = `aa43f0596ff095c3dad63e8ba10d6ae6`;
 const FORECAST_WEATHER_API = `http://api.openweathermap.org/data/2.5/forecast?`;
@@ -38,6 +41,25 @@ function searchLocation(forecastLoc) {
       return resp.json();
     })
     .then(function (resp) {
-      console.log(resp);
+      showCurrentDayForecast(resp);
     });
+}
+
+function showCurrentDayForecast(forecast) {
+  const tempText = currentDayForecast.children[0];
+  const humidText = currentDayForecast.children[1];
+  const windText = currentDayForecast.children[2];
+
+  currentDayLocation.textContent = `${forecast.city.name}, ${forecast.city.country} (Today)`;
+  tempText.textContent = `Temp: ${forecast.list[0].main.temp}°`;
+  humidText.textContent = `Humidity: ${forecast.list[0].main.humidity}%`;
+  windText.textContent = `Wind: ${forecast.list[0].wind.speed} m/s`;
+}
+
+function showExtraDaysForecast(forecast) {
+  for (let i = 8; i < 40; i += 8) {
+    if (i > 39) i = 39; //Data gets sent up to 39 timeframes
+
+    const currentForecast = forecast.list[i];
+  }
 }
